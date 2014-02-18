9174c70cd1c5b1f6ae90743eebb4a4f58524d8da
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-02-18 12:56:47 -0500

    Added some default sass and js files to show how this should be structured

---
+++ src/js/lib/log.js
@@ -0,0 +1,3 @@
+module.exports = function (val) {
+  if (global && global.console && global.console.log) global.console.log(val);
+};
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var log = require("./lib/log"),
+    $ = require("./vendor/jquery");
+
+
+$(function () {
+
+  log("The document is ready and we have some js running, way to go. Big accomplishment. We're all pulling for you.");
+
+});
