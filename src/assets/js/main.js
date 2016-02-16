8fe7087b4f22600535584e2ae313c7c42258c351
Eric Conrad <eric.jeffrey.conrad@gmail.com>
2016-02-15 22:27:29 -0500

    added layout-debugger html to default.html, added triggering js
    would much rather this be included as a partial or include

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,3 +7,38 @@ $(function () {
   log("The document is ready and we have some js running, way to go. Big accomplishment. We're all pulling for you.");
 
 });
+
+$(function() {
+  var first = false;
+
+  function toggleBp() {
+    $("body").toggleClass("debug-breakpoints");
+    first = false;
+  }
+
+  $("body").on("keypress", function (e) {
+
+    // If key pressed is "b"
+    if (e.which === 98) {
+
+      // If already been pressed, do action
+      if (first) {
+        toggleBp();
+      }
+
+      else {
+
+        // On first press, set first true
+        first = true;
+
+        // Reset first so that keypresses must
+        // happen in quick succession
+        setTimeout(function () {
+          first = false;
+        }, 250);
+
+      }
+    }
+
+  });
+});
